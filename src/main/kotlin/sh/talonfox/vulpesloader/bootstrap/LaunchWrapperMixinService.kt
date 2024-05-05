ab8535c84e91dee335412eb7d37ea52e45691e5e
Talon Kettuso <105325988+TalonFox@users.noreply.github.com>
2024-05-05 09:47:04 -0500

    Allow Java 17 mixins to be used

---
+++ src/main/kotlin/sh/talonfox/vulpesloader/bootstrap/LaunchWrapperMixinService.kt
@@ -0,0 +1,11 @@
+package sh.talonfox.vulpesloader.bootstrap
+
+import org.spongepowered.asm.mixin.MixinEnvironment.CompatibilityLevel
+import org.spongepowered.asm.service.mojang.MixinServiceLaunchWrapper
+
+
+class LaunchWrapperMixinService : MixinServiceLaunchWrapper() {
+    override fun getMaxCompatibilityLevel(): CompatibilityLevel {
+        return CompatibilityLevel.JAVA_17
+    }
+}
