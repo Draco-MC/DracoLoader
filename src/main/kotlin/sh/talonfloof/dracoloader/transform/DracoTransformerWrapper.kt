1ccda69175311767907e162814a23e8fddd0f6f8
Talon Kettuso <105325988+TalonFox@users.noreply.github.com>
2024-06-05 10:49:41 -0500

    Reverted the Reentrance lock change

--- src/main/kotlin/sh/talonfloof/dracoloader/transform/DracoTransformerWrapper.kt
+++ src/main/kotlin/sh/talonfloof/dracoloader/transform/DracoTransformerWrapper.kt
@@ -7,7 +7,7 @@ import org.spongepowered.asm.service.MixinService
 class DracoTransformerWrapper(private val transformer: IDracoTransformer) : IClassTransformer {
     override fun transform(name: String, transformedName: String?, basicClass: ByteArray?): ByteArray? {
         val ret = transformer.transform(Launch.classLoader, name, basicClass)
-        MixinService.getService().reEntranceLock.clear()
+        //MixinService.getService().reEntranceLock.clear()
         return ret
     }
 }
